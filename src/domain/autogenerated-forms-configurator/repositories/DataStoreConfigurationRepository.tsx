550434648f76a8a36afca1f6aa9df1e5ccd509fb
deeonwuli <onwulidumebi@gmail.com>
2023-09-08 20:49:29 +0100

    feat: basic functional editor

--- src/domain/autogenerated-forms-configurator/repositories/DataStoreConfigurationRepository.tsx
+++ src/domain/autogenerated-forms-configurator/repositories/DataStoreConfigurationRepository.tsx
@@ -1,5 +1,6 @@
 import { DataSet } from "../entities/DataSet";
 
 export interface DataStoreConfigurationRepository {
+    getFormConfig(namespace: string): Promise<any>;
     getDatasets(): Promise<DataSet[]>;
 }
