b0d445b05ae782919f968e96e52f02c64a2b5884
fdelemarre <florian.delemarre@365talents.com>
2024-05-21 09:56:03 +0200

    [Feature] Add test to DataForm

---
+++ src/domain/common/entities/__tests__/dataFixtures.ts
@@ -0,0 +1,51 @@
+import { DataForm, SectionBase, defaultTexts } from "../DataForm";
+import { DataElement } from "../DataElement";
+
+const dataElementBase: DataElement = {
+    id: "1",
+    code: "1",
+    name: `Element 1`,
+    type: "TEXT",
+    categoryCombos: {
+        id: "1",
+        name: "Combo",
+        categoryOptionCombos: [
+            {
+                id: "1",
+                name: "Option Combo",
+                shortName: "OC",
+                formName: undefined,
+            },
+        ],
+    },
+    categoryOptionCombos: [],
+    rules: [],
+    htmlText: undefined,
+    related: undefined,
+};
+
+export const sectionBase: Omit<SectionBase, "id" | "name" | "viewType"> = {
+    toggle: { type: "none" },
+    texts: defaultTexts,
+    tabs: { active: true },
+    sortRowsBy: "name",
+    titleVariant: "h1",
+    styles: { title: {}, columns: {}, rows: {}, totals: {} },
+    columnsDescriptions: undefined,
+    groupDescriptions: undefined,
+    disableComments: false,
+    showRowTotals: false,
+    toggleMultiple: [],
+    indicators: [],
+    dataElements: [dataElementBase],
+};
+
+export const dataFormBase: Omit<DataForm, "sections"> = {
+    id: "1",
+    expiryDays: 0,
+    dataInputPeriods: [],
+    dataElements: [],
+    texts: defaultTexts,
+    options: { dataElements: {} },
+    indicators: [],
+};
