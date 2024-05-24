0744010550d298f6618849163489d6d354b8ee30
fdelemarre <florian.delemarre@365talents.com>
2024-05-24 11:49:15 +0200

    test ApplyToAllUseCase

---
+++ src/domain/common/usecases/__tests__/data/dataValue.ts
@@ -0,0 +1,61 @@
+import { DataElement } from "../../../entities/DataElement";
+import { DataValueNumberSingle, DataValueTextMultiple, DataValueTextSingle } from "../../../entities/DataValue";
+
+export const dataElement: Omit<DataElement, "type"> = {
+    id: "1",
+    code: "DE1",
+    name: "Element 1",
+    categoryCombos: {
+        id: "1",
+        name: "Combo",
+        categoryOptionCombos: [
+            {
+                id: "1",
+                name: "Option Combo",
+                shortName: "OC",
+                formName: undefined,
+            },
+        ],
+    },
+    categoryOptionCombos: [],
+    rules: [],
+    htmlText: undefined,
+    related: undefined,
+};
+
+export const dataValueText: DataValueTextSingle = {
+    dataElement: { ...dataElement, type: "TEXT" },
+    period: "202101",
+    orgUnitId: "ou1",
+    categoryOptionComboId: "coc1",
+    isMultiple: false,
+    type: "TEXT",
+    value: "10",
+};
+
+export const dataValueTextMultiple: DataValueTextMultiple = {
+    dataElement: { ...dataElement, id: "de1", code: "de1", type: "TEXT" },
+    period: "202101",
+    orgUnitId: "ou1",
+    categoryOptionComboId: "coc1",
+    values: ["value1", "value2"],
+    type: "TEXT",
+    isMultiple: true,
+};
+
+export const dataValueNumberSingle: DataValueNumberSingle = {
+    dataElement: {
+        ...dataElement,
+        id: "de2",
+        code: "de2",
+        name: "Data Element 2",
+        type: "NUMBER",
+        numberType: "NUMBER",
+    },
+    period: "202101",
+    orgUnitId: "ou1",
+    categoryOptionComboId: "coc1",
+    value: "10",
+    type: "NUMBER",
+    isMultiple: false,
+};
