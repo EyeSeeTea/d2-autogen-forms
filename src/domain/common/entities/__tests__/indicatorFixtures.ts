f74543436441e8b2b59b33818702d9d4eb56062c
Chukwudumebi Onwuli <37223065+deeonwuli@users.noreply.github.com>
2025-09-25 12:14:57 +0100

    fix failing tests

--- src/domain/common/entities/__tests__/indicatorFixtures.ts
+++ src/domain/common/entities/__tests__/indicatorFixtures.ts
@@ -6,6 +6,7 @@ export const indicatorAfter: Indicator = {
     description: "Indicator 1",
     formula: "formula_1",
     dataElement: { code: "DE_1", direction: "after" },
+    name: "",
 };
 
 export const indicatorBefore: Indicator = {
@@ -14,6 +15,7 @@ export const indicatorBefore: Indicator = {
     description: "Indicator 2",
     formula: "formula_2",
     dataElement: { code: "DE_2", direction: "before" },
+    name: "",
 };
 
 export const indicators: Indicator[] = [indicatorAfter, indicatorBefore];
