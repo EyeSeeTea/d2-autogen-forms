3de7eb4ff889e09b915a492be5e483d6553758cd
Chukwudumebi Onwuli <37223065+deeonwuli@users.noreply.github.com>
2026-07-13 20:50:05 +0300

    refactor(data): rename filesystem repositories to *FsRepository

--- src/data/ExportConstantFsRepository.ts
+++ src/data/ExportConstantFsRepository.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import { Constant } from "../domain/common/entities/Constant";
 import { ExportConstantRepository } from "../domain/common/repositories/ExportConstantRepository";
 
-export class ExportConstantJsonRepository implements ExportConstantRepository {
+export class ExportConstantFsRepository implements ExportConstantRepository {
     async export(path: string, constants: Constant[]): Promise<void> {
         fs.writeFileSync(path, JSON.stringify({ constants }, null, 4));
     }
